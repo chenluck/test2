b293763b416c3cf4685d6d027eb813e58a6d66a1
cxl_k <cxl_k@163.com>
2016-10-10 20:52:43 +0800

    update

--- ttt/startup.h
+++ ttt/startup.h
@@ -7,6 +7,7 @@
  * tag1.1.1
  * tag1.2.0
  * tag1.2.1 @ branch_test2
+ * tag1.2.2 @ branch_test2
  * o Redistributions of source code must retain the above copyright notice, this list
  *   of conditions and the following disclaimer.
  *
